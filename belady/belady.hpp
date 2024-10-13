aca23359e186dd495257a97cfc7b33cfdb7c89e3
krutoi-muzhik <ddaniild@list.ru>
2024-10-13 21:39:30 +0300

    add predicting belady cache

---
+++ belady/belady.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <vector>
+#include <unordered_map>
+#include <cassert>
+
+
+template <typename KeyT = int>
+class Belady {
+ public:
+  Belady(const Belady&) = delete;
+  Belady operator=(const Belady&) = delete;
+
+  Belady(size_t size, std::vector<KeyT>& keys)
+    : size_(size),
+      keys_(keys) { }
+
+  bool hit(int idx) {
+    for (int i = 0; i < cache_.size(); i++) {
+      if (cache_[i] == keys_[idx])
+        return true;
+    }
+    return false;
+  }
+
+  int count_hits() {
+    int hits = 0;
+    for (int idx = 0; idx < keys_.size(); idx++) {
+      // check for hit
+      if (hit(idx)) {
+        hits++;
+        continue;
+      }
+
+      // check if key can be placed to cache
+      if (cache_.size() < size_) {
+        cache_.push_back(keys_[idx]);
+        continue;
+      }
+
+      // count replaced element
+      int far = idx;
+      int replace = -1;
+      for (int i = 0; i < cache_.size(); i++) {
+        int j;
+        for (j = idx + 1; j < keys_.size(); j++) {
+          if (cache_[i] == keys_[j] && j > far) {
+            far = j;
+            replace = i;
+            break;
+          }
+        }
+        if (j == keys_.size()) {
+          replace = i;
+          break;
+        }
+      }
+      
+      assert(replace >= 0);
+      cache_[replace] = keys_[idx];
+    }
+
+    return hits;
+  }
+
+ private:
+  size_t size_;
+  std::vector<KeyT> cache_;
+  std::vector<KeyT>& keys_;
+};
